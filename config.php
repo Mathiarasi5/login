f0427c664bae6df4e19a5a3484810821ed748c0a
Mathiarasi5 <69353674+Mathiarasi5@users.noreply.github.com>
2020-09-28 07:46:18 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$connect= new mysqli("$host","$user","$pass","login") or die("not connected");
+
+// if($connect===false)
+//  {
+//      echo("not connected");
+//  }
+//  else{
+   
+//     echo(" db is  connected");
+// }
+
+
+?>
